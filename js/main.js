d0e89f64d040529781eda39b318021a8fb0f1da8
y.glagolev <y.glagolev@artjoker.net>
2023-09-10 21:08:27 +0300

    fix hidden container

--- js/main.js
+++ js/main.js
@@ -1,70 +1,3 @@
-// const dateSelectElement = document.getElementById('date__select');
-// const zoomImages = document.querySelectorAll('.zoom__img');
-// let magnifiedImage = document.querySelector(".large__img");
-// const originalImage = document.querySelector(".zoom__img");
-// let originalImageWidth = originalImage.width;
-// let originalImageHeight = originalImage.height;
-//
-// function updateMagnifiedImage(mouseX, mouseY) {
-//   const magnifiedImageStyle = magnifiedImage.style;
-//   let xPercent = (mouseX / originalImageWidth) * 100;
-//   let yPercent = (mouseY / originalImageHeight) * 100;
-//
-//   if (mouseX > 0.01 * originalImageWidth) {
-//     xPercent += 0.15 * xPercent;
-//   }
-//
-//   if (mouseY >= 0.01 * originalImageHeight) {
-//     yPercent += 0.15 * yPercent;
-//   }
-//
-//   magnifiedImageStyle.backgroundPositionX = xPercent - 9 + "%";
-//   magnifiedImageStyle.backgroundPositionY = yPercent - 9 + "%";
-//   magnifiedImageStyle.left = mouseX - 69 + "px";
-//   magnifiedImageStyle.top = mouseY - 69 + "px";
-//   magnifiedImageStyle.opacity = 1;
-// }
-//
-// function hideMagnifiedImage() {
-//   const magnifiedImageStyle = magnifiedImage.style;
-//   magnifiedImageStyle.opacity = 0;
-// }
-//
-// dateSelectElement.addEventListener('change', function() {
-//   const selectedDate = dateSelectElement.value;
-//
-//   zoomImages.forEach((image) => {
-//     if (image.getAttribute('data-id') === selectedDate) {
-//       const zoomUrl = image.getAttribute('src').split("\\")
-//       image.style.display = 'block';
-//       magnifiedImage.style.background = `url(images/${zoomUrl[zoomUrl.length-1]}) no-repeat #ffffff`
-//     } else {
-//       image.style.display = 'none';
-//     }
-//   });
-// });
-//
-// const zoomContainer = document.getElementById("zoom");
-// zoomContainer.addEventListener("mousemove", function (e) {
-//   const mouseX = e.pageX - this.offsetLeft;
-//   const mouseY = e.pageY - this.offsetTop;
-//   updateMagnifiedImage(mouseX, mouseY);
-// }, false);
-//
-// zoomContainer.addEventListener("mouseout", function () {
-//   hideMagnifiedImage();
-// }, false);
-//
-// zoomContainer.addEventListener("touchmove", function (e) {
-//   const touch = e.touches[0];
-//   const mouseX = touch.clientX - this.offsetLeft;
-//   const mouseY = touch.clientY - this.offsetTop;
-//   updateMagnifiedImage(mouseX, mouseY);
-// }, false);
-//
-// zoomContainer.addEventListener("touchend", function () {
-//   hideMagnifiedImage();
-// }, false);
 const dateSelectElement = document.getElementById('date__select');
 const zoomImages = document.querySelectorAll('.zoom__img');
 let magnifiedImage = document.querySelector(".large__img");
@@ -144,19 +77,15 @@ zoomContainer.addEventListener("touchend", function () {
   hideMagnifiedImage();
 }, false);
 
-// Чтобы временно заблокировать прокрутку, установите scrollEnabled в false
 function disableScroll() {
   scrollEnabled = false;
   body.style.overflow = "hidden";
 }
 
-// Чтобы разрешить прокрутку, установите scrollEnabled в true
 function enableScroll() {
   scrollEnabled = true;
   body.style.overflow = "auto";
 }
-// Блокируем скроллинг при взаимодействии с изображением
 zoomContainer.addEventListener("touchstart", disableScroll, false);
 
-// Разблокируем скроллинг после взаимодействия с изображением
 zoomContainer.addEventListener("touchend", enableScroll, false);
